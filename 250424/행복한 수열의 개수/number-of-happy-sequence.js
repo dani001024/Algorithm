175c509052fca7f2e1b2c70d75b7efdc8d4aa5f9
dani001024 <77667199+dani001024@users.noreply.github.com>
2025-04-25 16:19:00 +0900

    Title: 행복한 수열의 개수, Time: 178ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250424/행복한 수열의 개수/number-of-happy-sequence.js
+++ 250424/행복한 수열의 개수/number-of-happy-sequence.js
@@ -14,7 +14,7 @@ for(var i=0; i<n; i++){
         if(grid[i][j] == grid[i][j-1]) row_cnt ++;
         else if(grid[i][j] != grid[i][j-1]) row_cnt =1
         if(grid[j][i] == grid[j-1][i]) col_cnt ++;
-        else if(grid[j][i] != grid[j-1][i]) row_cnt = 1;
+        else if(grid[j][i] != grid[j-1][i]) col_cnt = 1;
     }
     if(row_cnt>=m) answer ++;
     if(col_cnt>=m) answer ++;
