ce736db2937110212f7f95bafcc776aa4be71cf6
dani001024 <77667199+dani001024@users.noreply.github.com>
2025-04-25 16:19:11 +0900

    Title: 트로미노, Time: 164ms, Memory: 14MB, Status: Runtime Error - Codetree

--- 250424/트로미노/tromino.js
+++ 250424/트로미노/tromino.js
@@ -6,28 +6,26 @@ const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
 var answer = 0;
 
-for(var x=0; x<n-1; x++){
-    for(var y=0; y<m-1;y++){ //x,y기준
+for(var x=0; x<m-1; x++){
+    for(var y=0; y<n;y++){ //x,y기준 x가 세로 y가 가로
     var max = 0;
 
     // sum = grid[x][y] + grid[x][y+1];
     if(y-1>0) max = grid[x][y-1];
-    if(x-1>0 && max<grid[x-1][y]) max = grid[x-1][y];
-    if(x+1<m && max<grid[x+1][y]) max = grid[x+1][y];
-    if(x-1>0&&y+1<n&&max<grid[x-1][y+1]) max = grid[x-1][y+1];
-    if(x+1<m&&y+1<n&&max<grid[x+1][y+1]) max = grid[x+1][y+1];
-    if(y+2<n&&max<grid[x+1]) max = grid[x][y+2];
+    if(x-1>=0 && max<grid[x-1][y]) max = grid[x-1][y];
+    if(x+1<m-1 && max<grid[x+1][y]) max = grid[x+1][y];
+    if(x-1>0&&y+1<n-1&&max<grid[x-1][y+1]) max = grid[x-1][y+1];
+    if(x+1<m-1&&y+1<n-1&&max<grid[x+1][y+1]) max = grid[x+1][y+1];
+    if(y+2<n-1&&max<grid[x+1]) max = grid[x][y+2];
 
     answer = grid[x][y] + grid[x][y+1] + max > answer ? grid[x][y] + grid[x][y+1] + max :answer
-
     }
 }
-for(var x=0; x<n-2; x++){
+for(var x=0; x<m-2; x++){
     for(var y=0; y<n;y++){ //x,y기준
     var sum = grid[x][y] + grid[x+1][y] + grid[x+2][y]
 
     answer = sum > answer ? sum :answer
-
     }
 }
 console.log(answer)
