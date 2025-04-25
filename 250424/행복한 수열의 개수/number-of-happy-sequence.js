9bb4e72dfa70756c10c03a302dfba9d6663dbf43
dani001024 <77667199+dani001024@users.noreply.github.com>
2025-04-25 16:18:59 +0900

    Title: 행복한 수열의 개수, Time: 165ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250424/행복한 수열의 개수/number-of-happy-sequence.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+var answer = 0;
+
+
+for(var i=0; i<n; i++){
+    var row_cnt = 1;
+    var col_cnt =1;
+    for(var j=1; j<n; j++){
+        if(grid[i][j] == grid[i][j-1]) row_cnt ++;
+        else if(grid[i][j] != grid[i][j-1]) row_cnt =1
+        if(grid[j][i] == grid[j-1][i]) col_cnt ++;
+        else if(grid[j][i] != grid[j-1][i]) row_cnt = 1;
+    }
+    if(row_cnt>=m) answer ++;
+    if(col_cnt>=m) answer ++;
+}
+console.log(answer);
